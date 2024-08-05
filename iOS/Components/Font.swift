2d90651b0c613d132eb1315d712916eaf8a7bb1c
tonyfreeman <24860049+tonyfreeman@users.noreply.github.com>
2024-08-05 21:01:34 +0300

    Add custom fonts

---
+++ iOS/Components/Font.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+extension Font {
+    static func jetBrainsMonoRegular(size: Double) -> Font {
+        .custom("JetBrainsMono-Regular", size: size)
+    }
+
+    static func ppNeueMontrealRegular(size: Double) -> Font {
+        .custom("PPNeueMontreal-Regular", size: size)
+    }
+}
