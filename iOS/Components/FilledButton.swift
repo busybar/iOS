2a398bab861499a1a9942c6229a8983d765d1fda
tonyfreeman <24860049+tonyfreeman@users.noreply.github.com>
2024-08-06 02:52:43 +0300

    Fix color

--- iOS/Components/FilledButton.swift
+++ iOS/Components/FilledButton.swift
@@ -1,12 +1,15 @@
 import SwiftUI
 
-struct FilledButton: View {
-    let text: String
+struct FilledButton<Label: View>: View {
     let action: () -> Void
+    @ViewBuilder let label: () -> Label
 
-    init(_ text: String, action: @escaping () -> Void) {
-        self.text = text
+    init(
+        action: @escaping @MainActor () -> Void,
+        @ViewBuilder label: @escaping () -> Label
+    ) {
         self.action = action
+        self.label = label
     }
 
     var body: some View {
@@ -15,14 +18,23 @@ struct FilledButton: View {
         } label: {
             HStack {
                 Spacer()
-                Text(text)
+                label()
                     .font(.ppNeueMontrealRegular(size: 16))
-                    .foregroundColor(.white)
+                    .foregroundColor(.onColorWhite)
                     .padding(.vertical, 14)
                 Spacer()
             }
             .background(.accent)
             .cornerRadius(8)
+        }
+    }
+}
+
+extension FilledButton where Label == Text {
+    init(_ text: String, action: @escaping @MainActor () -> Void) {
+        self.action = action
+        self.label = {
+            Text(text)
         }
     }
 }
--- iOS/Content/DeviceView.swift
+++ iOS/Content/DeviceView.swift
@@ -2,9 +2,23 @@ import SwiftUI
 
 struct DeviceView: View {
     var body: some View {
-        ZStack {
-            Text("DeviceView")
+        VStack {
+            Image("BusyBar")
+                .resizable()
+                .scaledToFit()
+
+            FilledButton {
+            } label: {
+                HStack(spacing: 2) {
+                    Text("CHANGE APP")
+                    Image("Chevron")
+                }
+            }
+
+            Spacer()
         }
+        .padding(.top, 26)
+        .padding(.horizontal, 14)
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
